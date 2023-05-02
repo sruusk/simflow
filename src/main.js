826a12260143b3bd84b10dff4c89e6f2d85ab691
Apina-32 <samuli.ruuskanen@live.fi>
2023-05-02 06:28:01 +0300

    Remove registerServiceWorker.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import './registerServiceWorker';
-
 const app = createApp(App);
 const pinia = createPinia();
 
