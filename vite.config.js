3e9bbff97b81b4757f8356b82e6e8818bb85e204
Apina-32 <samuli.ruuskanen@live.fi>
2023-05-02 06:37:32 +0300

    Set base path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), VitePWA({
-        base: '/',
+        base: process.env.BASE_PATH || '/',
         srcDir: 'src',
         filename: 'sw.js',
         strategies: 'GenerateSW',
